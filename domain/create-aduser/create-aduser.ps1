6141d0faa7c82620621915660152b7ec0e3572ee
JB D. (Nuvodia) <jdyer@nuvodia.com>
2025-02-07 16:06:36 -0700

    Delete domain directory

--- domain/create-aduser/create-aduser.ps1
+++
@@ -1,5 +0,0 @@
-# Create the new user
-New-ADUser -Name "JB Dyer" -GivenName "JB" -Surname "Dyer" -SamAccountName "pa-jdyer" -UserPrincipalName "pa-jdyer@inlandtarp.com" -AccountPassword (ConvertTo-SecureString "6largehowlermonkeyS!" -AsPlainText -Force) -Enabled $true -Path "OU=Special Admins,DC=bch,DC=local"
-# Add user to Domain Admins AD group.
-Add-ADGroupMember -Identity "Domain Admins" -Members "pa-jdyer"
-
