07b353f175aa2fa225f904ad93564c67711fe2d1
JB D. (Nuvodia) <jdyer@nuvodia.com>
2025-02-07 16:05:08 -0700

    Delete 365 directory

--- 365/Get-FullMailboxAttributes/Get-FullMailboxAttributes.ps1
+++
@@ -1,9 +0,0 @@
-# Read the list of mailboxes from a text file
-$mailboxes = Get-Content "C:\Users\jdyer\OneDrive - Nuvodia\Documents\WindowsPowerShell\Scripts\getFullMailboxAttributes\mailboxes.txt"
-
-
-foreach ($mailbox in $mailboxes) {
-    $attributes = Get-Mailbox -Identity $mailbox | Select-Object *
-    $outputFile = "C:\Users\jdyer\OneDrive - Nuvodia\Documents\WindowsPowerShell\Scripts\getFullMailboxAttributes\$mailbox`_attributes.txt"
-    $attributes | Out-File $outputFile
-}
--- 365/diagnose-mailboxfolderassistant/diagnose-mailboxfolderassistant.ps1
+++
@@ -1,6 +0,0 @@
-$mailbox = "leadership@leadershipspokane.org"
-
-[xml]$diag = (Export-MailboxDiagnosticLogs $mailbox -ExtendedProperties).MailboxLog
-$diag.Properties.MailboxTable.Property | Where-Object {$_.Name -like "ELC*"}
-
-Export-MailboxDiagnosticLogs $mailbox -ComponentName MRM
