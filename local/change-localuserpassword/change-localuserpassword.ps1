5f3331f01cc5f4638f4709ecff9ad81ab8d14e3c
JB D. (Nuvodia) <jdyer@nuvodia.com>
2025-02-07 16:06:49 -0700

    Delete local directory

--- local/change-localuserpassword/change-localuserpassword.ps1
+++
@@ -1,5 +0,0 @@
-$Username = "username"
-$NewPassword = "Password123!"
-
-$SecurePassword = ConvertTo-SecureString $newpassword -AsPlainText -Force
-Set-LocalUser -Name $username -Password $SecurePassword
