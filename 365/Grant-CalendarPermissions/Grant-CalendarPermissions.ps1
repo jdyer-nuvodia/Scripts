98c8ab78478e0100931c56afb2b68d8010ad974e
jdyer-nuvodia <jdyer@nuvodia.com>
2025-02-14 10:08:48 -0700

    Update Grant-CalendarPermissions.ps1
    Update for swapping between user and group for granting permission.

--- 365/Grant-CalendarPermissions/Grant-CalendarPermissions.ps1
+++ 365/Grant-CalendarPermissions/Grant-CalendarPermissions.ps1
@@ -4,17 +4,20 @@ Import-Module ExchangeOnlineManagement
 # Connect to Exchange Online
 Connect-ExchangeOnline
 
-#Set variable for SG
-$securityGroupName = "ConfRmCal-Author"
+#Set variable for SG if adding to a group, be sure to change the $userName variable in the for loop to $securityGroupName
+#$securityGroupName = "ConfRmCal-Author"
 
-# Read the list of mailboxes from a text file
+#Set variable for user to receive access
+$userName = "mailbox"
+
+# Read the list of mailboxes from a text file, set this to the folder where this script 
 $mailboxes = Get-Content "C:\Users\jdyer\OneDrive - Nuvodia\Documents\WindowsPowerShell\Scripts\grantCalendarPermissions\mailboxes.txt"
 
 foreach ($mailbox in $mailboxes) {
     $identity = $mailbox.UserPrincipalName + ":\Calendar"
     
     try {
-        Set-MailboxFolderPermission -Identity $identity -User $securityGroupName -AccessRights CreateItems, ReadItems, FolderVisible -ErrorAction Stop
+        Set-MailboxFolderPermission -Identity $identity -User $userName -AccessRights CreateItems, ReadItems, FolderVisible -ErrorAction Stop
         Write-Host "Successfully granted $calendarPermission access to $($mailbox.UserPrincipalName)'s calendar for group $securityGroupName" -ForegroundColor Green
     }
     catch {
