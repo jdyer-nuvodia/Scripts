0345e3f03a494466a795f26f974dcedc96826e74
jdyer-nuvodia <jdyer@nuvodia.com>
2025-02-14 10:12:45 -0700

    Update Grant-CalendarPermissions.ps1

--- 365/Grant-CalendarPermissions/Grant-CalendarPermissions.ps1
+++ 365/Grant-CalendarPermissions/Grant-CalendarPermissions.ps1
@@ -11,13 +11,13 @@ Connect-ExchangeOnline
 $userName = "mailbox"
 
 # Read the list of mailboxes from a text file, set this to the folder where this script 
-$mailboxes = Get-Content "C:\Users\jdyer\OneDrive - Nuvodia\Documents\WindowsPowerShell\Scripts\grantCalendarPermissions\mailboxes.txt"
+$mailboxes = Get-Content "C:\Users\jdyer\OneDrive - Nuvodia\Documents\GitHub\Scripts\365\Grant-CalendarPermissions\mailboxes.txt"
 
 foreach ($mailbox in $mailboxes) {
     $identity = $mailbox.UserPrincipalName + ":\Calendar"
     
     try {
-        Set-MailboxFolderPermission -Identity $identity -User $userName -AccessRights CreateItems, ReadItems, FolderVisible -ErrorAction Stop
+        Set-MailboxFolderPermission -Identity $identity -User $userName -AccessRights Editor -SharingPermissionFlags Delegate,CanViewPrivateItems
         Write-Host "Successfully granted $calendarPermission access to $($mailbox.UserPrincipalName)'s calendar for group $securityGroupName" -ForegroundColor Green
     }
     catch {
